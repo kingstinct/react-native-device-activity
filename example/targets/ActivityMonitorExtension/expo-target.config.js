0d4f1380160e7360812bfc8219f2f72fbd4b673d
Robert Herber <robert@kingstinct.com>
2023-07-08 11:21:44 +0200

    Lets see if it’s easier in the original example

---
+++ example/targets/ActivityMonitorExtension/expo-target.config.js
@@ -0,0 +1,9 @@
+/** @type {import('expo-apple-targets/target-plugin/config').Config} */
+const config = {
+  type: "widget",
+  // accentColor: "steelblue",
+  // deploymentTarget: "9.4",
+  frameworks: ["DeviceActivity"],
+};
+
+module.exports = config;
