fcbaa4ceca631f92b400e0264fb0036d4779dbf7
Robert Herber <robert@kingstinct.com>
2023-07-12 22:49:43 +0200

    Fix application-groups entitlement

--- config-plugin/withEntitlements.js
+++ config-plugin/withEntitlements.js
@@ -6,8 +6,9 @@ const { withEntitlementsPlist } = require("expo/config-plugins");
 const withEntitlementsPlugin = (config) =>
   withEntitlementsPlist(config, (config) => {
     // todo: make this configurable - but would requiring changes in both Swift code and the /target/Info.plist to make sense
-    config.modResults["com.apple.security.application-groups"] =
-      "group.ActivityMonitor";
+    config.modResults["com.apple.security.application-groups"] = [
+      "group.ActivityMonitor",
+    ];
     config.modResults["com.apple.developer.family-controls"] = true;
 
     return config;
