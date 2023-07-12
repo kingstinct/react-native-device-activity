65a5a57fcbec13031f384b8ee6e24fd2037045e0
Robert Herber <robert@kingstinct.com>
2023-07-12 14:22:19 +0200

    Add entitlements to plugin, break out plugins

--- app.plugin.js
+++ app.plugin.js
@@ -2,31 +2,17 @@
 const withTargetPlugin =
   require("@kingstinct/expo-apple-targets/target-plugin/build").default;
 const { createRunOncePlugin } = require("expo/config-plugins");
-const fs = require("fs");
 
+const withCopyTargetFolder = require("./config-plugin/withCopyTargetFolder");
+const withEntitlementsPlugin = require("./config-plugin/withEntitlements");
 const pkg = require("./package.json");
-
-/** @type {import('@expo/config-plugins').ConfigPlugin} */
-const withCopyTargetFolder = (config) => {
-  const projectRoot = config._internal.projectRoot;
-  // eslint-disable-next-line no-undef
-  const packageTargetFolderPath = __dirname + "/targets";
-  const projectTargetFolderPath = projectRoot + "/targets";
-
-  if (!fs.existsSync(projectTargetFolderPath)) {
-    fs.mkdirSync(projectTargetFolderPath);
-  }
-
-  fs.cpSync(packageTargetFolderPath, projectTargetFolderPath, {
-    recursive: true,
-  });
-
-  return config;
-};
 
 /** @type {import('@expo/config-plugins').ConfigPlugin<{ appleTeamId: string; match?: string; }>} */
 const withActivityMonitorExtensionPlugin = (config, props) => {
-  return withCopyTargetFolder(withTargetPlugin(config, props));
+  return withTargetPlugin(
+    withEntitlementsPlugin(withCopyTargetFolder(config)),
+    props,
+  );
 };
 
 module.exports = createRunOncePlugin(
