6e62046f4df361978422e96544b391cb6c40da29
Robert Herber <robert@kingstinct.com>
2024-10-27 02:46:42 +0100

    Got expo targets building the extensions, didnt get to add ManagedSettings as frameworks, not sure if its a problem

---
+++ example/targets/ShieldAction/expo-target.config.js
@@ -0,0 +1,6 @@
+/** @type {import('expo-apple-targets/target-plugin/config').Config} */
+const config = {
+  type: "shield-action",
+};
+
+module.exports = config;
---
+++ targets/ShieldConfiguration/expo-target.config.js
@@ -0,0 +1,6 @@
+/** @type {import('expo-apple-targets/target-plugin/config').Config} */
+const config = {
+  type: "shield-configuration",
+};
+
+module.exports = config;
